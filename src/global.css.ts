61c61312ce4e06794c8a2875dfecc12336f2b4bc
Hyunsu Lim <hyunsu.lim01@gmail.com>
2025-04-30 18:35:57 +0900

    Add styling tool

---
+++ src/global.css.ts
@@ -0,0 +1,18 @@
+import { globalStyle } from "@vanilla-extract/css";
+
+globalStyle("body", {
+  margin: 0,
+  padding: 0,
+  overflow: "hidden",
+  width: "100%",
+  height: "100%",
+});
+
+globalStyle("#app", {
+  width: "100%",
+  height: "100%",
+  position: "absolute",
+  overflow: "hidden",
+  display: "flex",
+  flexDirection: "column",
+});
